7bc3f0043afcd4ae0a3675999f12db9b7bfd4fcd
Mauro Peixoto <mauroaugustodev@gmail.com>
2026-03-29 19:14:12 -0300

    Transferencia bancaria

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
 	}
 }
 
-func (c ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
 	if valorDoDeposito <= 0 {
 		return "Valor do depósito deve ser maior que zero", c.saldo
 	}
@@ -29,19 +29,24 @@ func (c ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
 	return "Depósito realizado com sucesso", c.saldo
 }
 
+func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+
+}
+
 func main() {
 
-	contaDoSilvia := ContaCorrente{}
-	contaDoSilvia.titular = "Silvia"
-	contaDoSilvia.numeroAgencia = 863
-	contaDoSilvia.numeroConta = 863452
-	contaDoSilvia.saldo = 1000.00
-
-	fmt.Println(contaDoSilvia.Sacar(300))
-	status, saldo := contaDoSilvia.Depositar(500)
-	fmt.Println(status)
-	fmt.Printf("Saldo atual: R$ %.2f\n", saldo)
-
-	fmt.Println("contado do Silvia tem saldo de R$", contaDoSilvia.saldo)
+	contaDoSilvia := ContaCorrente{titular: "Silvia", saldo: 300}
+	contaDoGustavo := ContaCorrente{titular: "Gustavo", saldo: 100}
+	status := contaDoSilvia.Transferir(200, &contaDoGustavo)
+	fmt.Printf("Status da transferencia: %t\n", status)
+	fmt.Printf("Saldo da conta do Silvia: %.2f\n", contaDoSilvia.saldo)
+	fmt.Printf("Saldo da conta do Gustavo: %.2f\n", contaDoGustavo.saldo)
 
 }
